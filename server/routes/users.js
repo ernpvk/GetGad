f693fc3e1f98828b408ca4eadcdbb1ef70231821
earaen <123929693+earaen@users.noreply.github.com.>
2025-01-14 05:40:56 +0700

    feat: add edit user's info router

--- server/routes/users.js
+++ server/routes/users.js
@@ -27,4 +27,19 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+// Edit user's info
+router.put("/:id", async (req, res, next) => {
+  try {
+    const response = await fetch(`https://fakestoreapi.in/api/users/${req.params.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body),
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
