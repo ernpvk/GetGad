1dd09e77b7e16bbb6f43e5e94f39e1b52e4d9d66
earaen <123929693+earaen@users.noreply.github.com.>
2025-01-17 08:07:27 +0700

    feat: configure tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,98 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3182CE",
+          light: "#63B3ED",
+          dark: "#2C5282",
+        },
+        secondary: {
+          DEFAULT: "#718096",
+          light: "#A0AEC0",
+          dark: "#4A5568",
+        },
+        background: {
+          primary: "#F7FAFC",
+          secondary: "#EDF2F7",
+          dark: "#1A202C",
+        },
+        surface: {
+          light: "#FFFFFF",
+          dark: "#2D3748",
+        },
+
+        text: {
+          primary: "#2D3748",
+          secondary: "#718096",
+          light: "#F7FAFC",
+        },
+      },
+
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+        heading: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        xs: ["0.75rem", "1rem"],
+        sm: ["0.875rem", "1.25rem"],
+        base: ["1rem", "1.5rem"],
+        lg: ["1.125rem", "1.75rem"],
+        xl: ["1.25rem", "1.75rem"],
+        "2xl": ["1.5rem", "2rem"],
+        "3xl": ["1.875rem", "2.25rem"],
+        "4xl": ["2.25rem", "2.5rem"],
+      },
+      fontWeight: {
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+      },
+      spacing: {
+        px: "1px",
+        0: "0",
+        0.5: "0.125rem",
+        1: "0.25rem",
+        2: "0.5rem",
+        3: "0.75rem",
+        4: "1rem",
+        5: "1.25rem",
+        6: "1.5rem",
+        8: "2rem",
+        10: "2.5rem",
+        12: "3rem",
+        16: "4rem",
+      },
+
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+        },
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1536px",
+        },
+      },
+
+      borderRadius: {
+        none: "0",
+        sm: "0.125rem",
+        DEFAULT: "0.25rem",
+        md: "0.375rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        "2xl": "1rem",
+        full: "9999px",
+      },
+    },
   },
   plugins: [],
 };
